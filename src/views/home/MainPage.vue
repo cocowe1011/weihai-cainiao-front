6be3ffcca30f0307583bbbf8cc3747e66dc7ec60
cocowe1011 <958486729@qq.com>
2026-09-12 15:50:13 +0800

    改成1S

--- src/views/home/MainPage.vue
+++ src/views/home/MainPage.vue
@@ -2609,7 +2609,7 @@ export default {
         );
       }
     },
-    // 不进上货队列：给PLC发剔除命令 DBW118=1，保持500ms后取消
+    // 不进上货队列：给PLC发剔除命令 DBW118=1，保持1秒后取消
     rejectScanNotEnqueue(reason) {
       const cmdAdd = 'W_DBW118';
       ipcRenderer.send('writeSingleValueToPLC', cmdAdd, 1);
@@ -2619,9 +2619,9 @@ export default {
       this._plcRejectCancelTimer = setTimeout(() => {
         ipcRenderer.send('cancelWriteToPLC', cmdAdd);
         this._plcRejectCancelTimer = null;
-      }, 500);
+      }, 1000);
       this.addLog(
-        `${reason}，不进入上货队列，已发剔除命令 ${cmdAdd}=1（保持500ms）`,
+        `${reason}，不进入上货队列，已发剔除命令 ${cmdAdd}=1（保持1秒）`,
         'alarm'
       );
     },
